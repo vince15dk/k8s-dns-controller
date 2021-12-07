33fac7c076ad60e5b2ce16b14a2d46698eacaeeb
vince15dk <vince15dk@gmail.com>
2021-12-07 23:29:32 +0900

    update testing main for rest config

--- main.go
+++ main.go
@@ -1,33 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/vince15dk/k8s-operator-ingress/pkg/controller"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"log"
 	"time"
 )
 
-func main() {
-	//var kubeconfig *string
-	//kubeconfig = flag.String("kubeconfig", "/Users/nhn/.kube/config", "location to your kubeconfig file")
-	//config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	//if err != nil{
-	//	// handle error
-	//	fmt.Printf("error %s, building config from flags\n", err.Error())
-	//	config, err = rest.InClusterConfig()
-	//	if err != nil{
-	//		fmt.Printf("error %s, getting inclusterconfig", err.Error())
-	//	}
-	//}
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		fmt.Printf("error %s, getting inclusterconfig", err.Error())
+func main(){
+	var kubeconfig *string
+	kubeconfig = flag.String("kubeconfig", "/Users/nhn/.kube/config", "location to your kubeconfig file")
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil{
+		// handle error
+		fmt.Printf("error %s, building config from flags\n", err.Error())
+		config, err = rest.InClusterConfig()
+		if err != nil{
+			fmt.Printf("error %s, getting inclusterconfig", err.Error())
+		}
 	}
+
 	clientSet, err := kubernetes.NewForConfig(config)
-	if err != nil {
+	if err != nil{
 		log.Printf("getting std client %s\n", err.Error())
 	}
 
@@ -40,7 +39,7 @@ func main() {
 	// start initializes all requested informers
 	informer.Start(ch)
 
-	if err := c.Run(ch); err != nil {
+	if err := c.Run(ch); err != nil{
 		log.Printf("error running controller %s\n", err.Error())
 	}
 }
